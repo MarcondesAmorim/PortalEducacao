87c79206940cc42b9c8ddf4ee81dda91d20f5b7f
Marcondes Amorim <marcondesjra@gmail.com>
2019-11-05 14:44:47 -0300

    first commit

---
+++ src/br/com/pe/curso/PrimeiroServlet.java
@@ -0,0 +1,20 @@
+package br.com.pe.curso;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class PrimeiroServlet extends HttpServlet{
+
+	@Override
+	protected void doGet(
+		HttpServletRequest request,
+		HttpServletResponse resp) 
+			throws  ServletException, IOException {
+				System.out.println(" Olá mundo");
+	}
+}
